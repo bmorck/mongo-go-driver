3bc000df6e417015af8c209bb7c60e35a6e2b1d8
Bobby Morck <rmorck3098@gmail.com>
2022-03-21 15:25:45 -0400

    MHOUSE-logseed: Change to fmt.println

--- x/mongo/driver/uuid/uuid.go
+++ x/mongo/driver/uuid/uuid.go
@@ -7,6 +7,7 @@
 package uuid // import "go.mongodb.org/mongo-driver/x/mongo/driver/uuid"
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"math/rand"
@@ -32,9 +33,9 @@ var random = randutil.NewLockedRand(rand.NewSource(seed))
 func New() (UUID, error) {
 	var uuid [16]byte
 
-	logger.Println("LOGGING SEED")
-	logger.Println(os.Getpid())
-	logger.Println(seed)
+	fmt.Println("LOGGING SEED")
+	fmt.Println(os.Getpid())
+	fmt.Println(seed)
 	logged = true
 
 	_, err := io.ReadFull(random, uuid[:])
